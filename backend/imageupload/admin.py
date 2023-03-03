19938efcd47cc9b0b77bccb25536252aeafc660b
raleung2 <70629703+raleung2@users.noreply.github.com>
2023-03-02 22:17:55 -0800

    reconfig git

--- backend/imageupload/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-# Register your models here.
-# registers post model to admin interface
-admin.site.register(Post)
-
--- backend/imageupload/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [
-    path('posts/', PostView.as_view(), name='post_list'),
-]
